be99ecc1c37bfd5a3a4bdd3764b833e5b1286482
pryceholmes <113060481+pryceholmes@users.noreply.github.com>
2025-02-28 10:47:10 -0600

    add fields, constructor, and getters and setters to DNDCharacter

--- app/src/main/java/com/dnd/model/DNDCharacter.java
+++ app/src/main/java/com/dnd/model/DNDCharacter.java
@@ -1,25 +1,89 @@
 package com.dnd.model;
 
 public class DNDCharacter {
-    // TODO: Define character attributes
+    // member variables
     private int id;
     private String name;
     private String race;
     private String characterClass;
     private String alignment;
     private int level;
+    private boolean isNpc;
+    private int strength;
+    private int dexterity;
+    private int constitution;
+    private int intelligence;
+    private int wisdom;
+    private int charisma;
+    private Location location;
 
-    // TODO: Implement constructor
-    public DNDCharacter(int id, String name, String race, String characterClass, String alignment, int level) {
+    // constructor
+    public DNDCharacter(int id, String name, String race, String characterClass, String alignment, int level, boolean isNpc, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma, Location location) {
         this.id = id;
         this.name = name;
         this.race = race;
         this.characterClass = characterClass;
         this.alignment = alignment;
         this.level = level;
+        this.isNpc = isNpc;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.constitution = constitution;
+        this.intelligence = intelligence;
+        this.wisdom = wisdom;
+        this.charisma = charisma;
+        this.location = location;
     }
 
-    // TODO: Generate Getters and Setters for all fields
+    // getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getCharacterClass() {
+        return characterClass;
+    }
+
+    public void setCharacterClass(String characterClass) {
+        this.characterClass = characterClass;
+    }
+
+    public String getAlignment() {
+        return alignment;
+    }
+
+    public void setAlignment(String alignment) {
+        this.alignment = alignment;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
 
     // TODO: Override toString() for easy display
     @Override
