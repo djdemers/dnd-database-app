e8349e318a46062e54162a0cba1a7574a89a5936
pryceholmes <113060481+pryceholmes@users.noreply.github.com>
2025-02-28 10:48:14 -0600

    add getters and setters for real

--- app/src/main/java/com/dnd/model/DNDCharacter.java
+++ app/src/main/java/com/dnd/model/DNDCharacter.java
@@ -84,6 +84,70 @@ public class DNDCharacter {
         this.level = level;
     }
 
+    public boolean isNpc() {
+        return isNpc;
+    }
+
+    public void setNpc(boolean isNpc) {
+        this.isNpc = isNpc;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public int getConstitution() {
+        return constitution;
+    }
+
+    public void setConstitution(int constitution) {
+        this.constitution = constitution;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getWisdom() {
+        return wisdom;
+    }
+
+    public void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
+    }
+
+    public int getCharisma() {
+        return charisma;
+    }
+
+    public void setCharisma(int charisma) {
+        this.charisma = charisma;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
 
     // TODO: Override toString() for easy display
     @Override
