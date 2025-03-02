cb24ddf1c93c12e951f1bebe0534bf50747e4eed
pryceholmes <113060481+pryceholmes@users.noreply.github.com>
2025-03-02 10:03:57 -0600

    Session log and Note models

--- app/src/main/java/com/dnd/model/Notes.java
+++ app/src/main/java/com/dnd/model/Notes.java
@@ -1,13 +1,39 @@
 package com.dnd.model;
 
 public class Notes {
-    // TODO: Define objective attributes
+    private int noteId;
+    private int sessionId;
+    private String noteText;
+    public Notes(int noteId, int sessionId, String noteText) {
+        this.noteId = noteId;
+        this.sessionId = sessionId;
+        this.noteText = noteText;
+    }
+    public int getNoteId() {
+        return noteId;
+    }
 
+    public void setNoteId(int noteId) {
+        this.noteId = noteId;
+    }
 
-    // TODO: Implement constructor
+    public int getSessionId() {
+        return sessionId;
+    }
 
+    public void setSessionId(int sessionId) {
+        this.sessionId = sessionId;
+    }
 
-    // TODO: Generate Getters and Setters for all fields
+    public String getNoteText() {
+        return noteText;
+    }
 
-    // TODO: Override toString() for easy output
+    public void setNoteText(String noteText) {
+        this.noteText = noteText;
+    }
+    @Override
+    public String toString() {
+        return String.format("Note ID: %d, Session ID: %d, Note Text: %s", noteId, sessionId, noteText);
+    }
 }
